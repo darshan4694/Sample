c5db3e35fcc38795d85e033ff0a63b6689254ca8
Darshan <darshan.smu@gmail.com>
2018-06-05 23:52:23 -0400

    Hello

--- 1.js
+++ 1.js
@@ -1,4 +1,4 @@
 console.log("Hello");
-console.log("From Unecessary here");
+console.log("From Unecessary here from sample 2");
 console.log("From Sample Feature");
 console.log("From Some change in Develop");
